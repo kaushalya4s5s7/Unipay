af094fbcc79ac313272311ae27de6cf8ca1e5de1
kaushalya4s5s7 <chaudharikaushal02@gmail.com>
2024-12-28 18:17:39 +0530

    solved issue 8

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const cors = require("cors");
 const express = require("express");
 const app = express();
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 const prefix = "/api/v1";
 
 // Allow multiple origins
