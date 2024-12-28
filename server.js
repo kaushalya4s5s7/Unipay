e213e1aa6553f68c374ed30e3f3251374b24c8e0
kaushalya4s5s7 <chaudharikaushal02@gmail.com>
2024-12-28 14:24:17 +0530

    Adding all the files

---
+++ server.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+// mongodb connection
+const mongoose = require("mongoose");
+const uri = process.env.DB_URI;
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => console.log(err));
+
+require("./app");
